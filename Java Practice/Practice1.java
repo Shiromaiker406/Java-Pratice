fab70563296d873ae5653c9f1949bc4d042ec754
Shiromaiker406 <kennethricaborda40@gmail.com>
2026-05-03 23:19:23 +0800

    Add files via upload

---
+++ Java Practice/Practice1.java
@@ -0,0 +1,25 @@
+public class Practice1 {
+
+    public static void main (String [] args) {
+
+
+        System.out.println("Hello Word");
+        System.out.println("123");
+        System.out.println(5 + 5);
+        System.out.println(6 * 9);
+        System.out.println(9 - 1);
+        System.out.println("Neso Academy Java Course!");
+        
+    }
+
+    public static void sayHi() {
+        System.out.println("Hello KENNETH");
+        System.out.println("Predator HElios 16");
+    }
+
+    public static void sayLow() {
+        System.out.println("I am a Genius");
+        System.out.println("Sorceros Supreme");
+    }
+    
+}
---
+++ Java Practice/Practice2.java
@@ -0,0 +1,7 @@
+public class Practice2 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Iam programmer");
+    }
+}
